90eb87c97a8f321f0369c6d277659539e397c340
NotNotRama <ramiroherrera.utn@gmail.com>
2024-08-25 19:45:47 +0000

    Change naming

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { useGetCountriesQuery, GetCountriesQuery } from '../generated/graphql';
 import dynamic from 'next/dynamic';
-import { SearchBar } from '@/components/SearchBar';
+import { SearchBar } from '@/components/search-bar';
 import { useFilteredCountries } from '@/hooks/useFilteredCountries';
+import { LoadingSpinner } from '@/components/ui/loading-spinner';
 
-const Map = dynamic(() => import('../components/Map').then((mod) => mod.Map), {
+const Map = dynamic(() => import('../components/map').then((mod) => mod.Map), {
   ssr: false,
 });
 
@@ -13,7 +14,7 @@ export default function Home() {
   const [searchTerm, setSearchTerm] = useState('');
   const filteredCountries = useFilteredCountries(data?.countries, searchTerm);
 
-  if (loading) return <p className="text-center p-4">Loading...</p>;
+  if (loading) return <LoadingSpinner />;
   if (error)
     return (
       <p className="text-center p-4 text-red-500">Error: {error.message}</p>
