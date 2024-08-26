c3b5fcaf33307698d4e2d630721394f4892374df
NotNotRama <ramiroherrera.utn@gmail.com>
2024-08-26 15:17:42 +0000

    Add LatLngExpression constants for South America and world center

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
 import { type ClassValue, clsx } from 'clsx';
+import { LatLngExpression } from 'leaflet';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const southAmericaCenter: LatLngExpression = [-14.235, -51.925];
+export const worldCenter: LatLngExpression = [0, 0];
